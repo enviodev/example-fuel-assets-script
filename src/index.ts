1d15cd04cd40a1041dbc0c8da96d6d63cf4198ba
Jason <jason@envio.dev>
2024-06-14 22:30:17 +0200

    Get code working just for calculating total supply of all assets on Fuel

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ import {
 } from "./config";
 import { getMintedAssetId } from "@fuel-ts/transactions"
 
-// Convert address to topic for filtering. Padds the address with zeroes.
-function addressToTopic(address: string): string {
-  return "0x000000000000000000000000" + address.slice(2, address.length);
-}
-
 const transferEventSigHash =
   "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
@@ -18,7 +13,6 @@ async function main() {
   // Create hypersync client using the mainnet hypersync endpoint
   const client = HyperfuelClient.new({
     url: hyperFuelEndpoint,
-    // maxNumRetries: 0,
   });
 
   const initialFromBlock = 518152; // First asset transaction on testnet.
@@ -29,7 +23,8 @@ async function main() {
       fromBlock: fromBlock,
       "receipts": [
         {
-          "receiptType": [7, 8, 11, 12]
+          // "receiptType": [7, 8, 11, 12] // Removing Transfer and TransferOut for now since it is unclear how to get tho from/sender address
+          "receiptType": [11, 12]
         }
       ],
       "fieldSelection": {
@@ -38,13 +33,15 @@ async function main() {
           "root_contract_id",
           "tx_id",
           "tx_status",
+          "to",
           "to_address",
           "amount",
           "asset_id",
           "val",
           "receipt_type",
-          "recipient",
-          "sub_id"
+          "sub_id",
+          "sender",
+          "recipient"
         ]
       }
     }
@@ -77,6 +74,8 @@ async function main() {
     }
   } = {};
 
+  const mintingContract: { [contractAddress: string]: Array<string> } = {};
+
   let fromBlock = initialFromBlock;
 
   let assetReceiptsProcessed = 0;
@@ -91,11 +90,11 @@ async function main() {
     fromBlock = result.nextBlock;
 
     for (const receipt of result.data.receipts) {
-      const { toAddress, val, amount, receiptType, recipient, rootContractId, subId } = receipt;
+      const { toAddress, to, val, amount, receiptType, recipient, rootContractId, subId, assetId } = receipt;
 
       if (receiptType === 11) {
         if (val == undefined || rootContractId == undefined || subId == undefined) {
-          throw new Error("Mulformed response from HyperFuel, required field cannot be undefined");
+          throw new Error("Malformed response from HyperFuel, required field cannot be undefined");
         }
 
         const assetId = getMintedAssetId(rootContractId, subId);
@@ -105,12 +104,13 @@ async function main() {
         if (asset) {
           asset.supply = asset.supply + val;
         } else {
+          const currentAssetsInContract = mintingContract[rootContractId] || [];
+          mintingContract[rootContractId] = [...currentAssetsInContract, assetId];
           asset = {
             subId: subId,
             supply: val,
             mintingContract: rootContractId,
             owners: {}
-
           }
         }
 
@@ -132,7 +132,7 @@ async function main() {
 
         // type 12 is a burn, so we need to subtract the value from the supply and do the reverse of the mint (11)
         if (val == undefined || rootContractId == undefined || subId == undefined) {
-          throw new Error("Mulformed response from HyperFuel, required field cannot be undefined");
+          throw new Error("Malformed response from HyperFuel, required field cannot be undefined");
         }
 
         if (rootContractId == "0x3550c53890db64a241d3cc6523d4255a9c588c4bd8503f911a39444989626626") {
@@ -163,12 +163,105 @@ async function main() {
           count_out: recipient.count_out + 1
         }
       } else if (receiptType === 7) {
-        // TODO
+        // UNUSED code - still buggy.
+        // Handle Transfer receipts
+        if (amount == undefined || assetId == undefined || to == undefined || rootContractId == undefined) {
+          console.log(receipt);
+          throw new Error("Malformed response from HyperFuel of type Transfer, required field cannot be undefined");
+        }
+
+        let asset = tokenAssets[assetId];
+        if (!asset) {
+          continue; // Ignore transfers for assets that were not minted
+        }
+
+        // Update recipient information
+        const recipientData = asset.owners[to];
+        if (recipientData) {
+          recipientData.in += amount;
+          recipientData.count_in += 1;
+        } else {
+          asset.owners[to] = {
+            in: amount,
+            out: BigInt(0),
+            count_in: 1,
+            count_out: 0
+          }
+        }
+
+        // Update sender information
+        const senderData = asset.owners[rootContractId];
+        if (senderData) {
+          senderData.out += amount;
+          senderData.count_out += 1;
+        } else {
+          asset.owners[rootContractId] = {
+            in: BigInt(0),
+            out: amount,
+            count_in: 0,
+            count_out: 1
+          }
+        }
       } else if (receiptType === 8) {
-        // TODO:
+        // UNUSED code - still buggy.
+        // Handle TransferOut receipts
+        if (amount == undefined || assetId == undefined || toAddress == undefined || rootContractId == undefined) {
+          throw new Error("Malformed response from HyperFuel of type TransferOut, required field cannot be undefined");
+        }
+
+        let asset = tokenAssets[assetId];
+        if (!asset) {
+          continue; // Ignore transfers for assets that were not minted
+        }
+
+        // Update recipient information
+        const recipientData = asset.owners[toAddress];
+        if (recipientData) {
+          recipientData.in += amount;
+          recipientData.count_in += 1;
+        } else {
+          asset.owners[toAddress] = {
+            in: amount,
+            out: BigInt(0),
+            count_in: 1,
+            count_out: 0
+          }
+        }
+
+        // Update sender information
+        const senderData = asset.owners[rootContractId];
+        if (senderData) {
+          senderData.out += amount;
+          senderData.count_out += 1;
+        } else {
+          asset.owners[rootContractId] = {
+            in: BigInt(0),
+            out: amount,
+            count_in: 0,
+            count_out: 1
+          }
+        }
       }
+    }
+  }
+
+  console.timeEnd("Script Execution Time");
+
+  console.log("Token Summary:");
+  for (const [contractAddress, assetIdsFromContract] of Object.entries(mintingContract)) {
+    console.log(`Minting Contract: ${contractAddress}`);
+
+    for (const assetId of assetIdsFromContract) {
+      const asset = tokenAssets[assetId];
+      if (!asset) {
+        throw new Error("Asset not found, logic error mismatch between contractAssets and tokenAssets objects");
+      }
+      console.log(`  - Sub ID: ${asset.subId}`);
+      console.log(`    Asset ID: ${assetId}`);
+      console.log(`    Supply: ${asset.supply}`);
     }
   }
 }
 
 main();
+
